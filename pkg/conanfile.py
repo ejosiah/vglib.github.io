0a4dd247fe5c3bc846706e78652d9e2dd79bcdcd
Josiah Ebhomenye <joebhomenye@gmail.com>
2022-12-29 20:05:49 +0000

    increment version number

--- pkg/conanfile.py
+++ pkg/conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class VgLibConan(ConanFile):
     name = "vglib"
-    version = "0.0.3"
+    version = "0.1.0"
     license = "<Put the package license here>"
     author = "Josiah Ebhomenye joebhomenye@gmail.com"
     url = "<Package recipe repository url here, for issues about the package>"
