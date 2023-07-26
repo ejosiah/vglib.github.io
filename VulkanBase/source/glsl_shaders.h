1d57db2dd269a62a2759ad9e704239510fffa928
Josiah Ebhomenye <joebhomenye@gmail.com>
2023-07-26 18:50:09 +0100

    moved shaders to central location

---
+++ VulkanBase/source/glsl_shaders.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <vector>
+
+extern std::vector<uint32_t> __glsl_add_comp_spv;
+extern std::vector<uint32_t> __glsl_scan_comp_spv;
+extern std::vector<uint32_t> __glsl_average_comp;
+extern std::vector<uint32_t> __glsl_reduce_output_comp_spv;
