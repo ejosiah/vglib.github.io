1fc0f65cf5bd89088de73c5dbd7f623f7751f1ec
Josiah Ebhomenye <joebhomenye@gmail.com>
2024-05-02 14:07:46 +0100

    added barrier functions for BufferRegion

--- VulkanBase/include/Barrier.hpp
+++ VulkanBase/include/Barrier.hpp
@@ -21,4 +21,20 @@ namespace Barrier {
 
     void transferReadToWrite(VkCommandBuffer commandBuffer, std::initializer_list<VulkanBuffer> buffers);
 
+    void fragmentReadToComputeWrite(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions);
+
+    void computeWriteToFragmentRead(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions);
+
+    void computeWriteToRead(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions);
+
+    void computeWriteToTransferRead(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions);
+
+    void transferWriteToRead(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions);
+
+    void transferWriteToComputeRead(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions);
+
+    void transferWriteToWrite(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions);
+
+    void transferReadToWrite(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions);
+
 }
--- VulkanBase/source/Barrier.cpp
+++ VulkanBase/source/Barrier.cpp
@@ -141,3 +141,146 @@ void Barrier::computeWriteToFragmentRead(VkCommandBuffer commandBuffer, std::ini
     vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0, 0,
                          VK_NULL_HANDLE, COUNT(barriers), barriers.data(), 0, VK_NULL_HANDLE);
 }
+
+
+void Barrier::computeWriteToRead(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions) {
+    std::vector<VkBufferMemoryBarrier> barriers(regions.size());
+
+    for(int i = 0; i < regions.size(); i++) {
+        auto region = std::next(regions.begin(), i);
+        barriers[i].sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+        barriers[i].srcAccessMask = VK_ACCESS_SHADER_WRITE_BIT;
+        barriers[i].dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+        barriers[i].offset = region->offset;
+        barriers[i].buffer = *region->buffer;
+        barriers[i].size = region->size();
+    }
+
+    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, 0, 0,
+                         VK_NULL_HANDLE, COUNT(barriers), barriers.data(), 0, VK_NULL_HANDLE);
+}
+
+void
+Barrier::computeWriteToTransferRead(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions) {
+    std::vector<VkBufferMemoryBarrier> barriers(regions.size());
+
+    for(int i = 0; i < regions.size(); i++) {
+        auto region = std::next(regions.begin(), i);
+        barriers[i].sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+        barriers[i].srcAccessMask = VK_ACCESS_SHADER_WRITE_BIT;
+        barriers[i].dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+        barriers[i].offset = region->offset;
+        barriers[i].buffer = *region->buffer;
+        barriers[i].size = region->size();
+    }
+
+    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0,
+                         VK_NULL_HANDLE, COUNT(barriers), barriers.data(), 0, VK_NULL_HANDLE);
+}
+
+void Barrier::transferWriteToRead(VkCommandBuffer commandBuffer,
+                                  std::initializer_list<BufferRegion> regions) {
+    std::vector<VkBufferMemoryBarrier> barriers(regions.size());
+
+    for(int i = 0; i < regions.size(); i++) {
+        auto region = std::next(regions.begin(), i);
+        barriers[i].sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+        barriers[i].srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barriers[i].dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+        barriers[i].offset = region->offset;
+        barriers[i].buffer = *region->buffer;
+        barriers[i].size = region->size();
+    }
+
+    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0,
+                         VK_NULL_HANDLE, COUNT(barriers), barriers.data(), 0, VK_NULL_HANDLE);
+
+}
+
+void Barrier::transferWriteToComputeRead(VkCommandBuffer commandBuffer,
+                                         std::initializer_list<BufferRegion> regions) {
+    std::vector<VkBufferMemoryBarrier> barriers(regions.size());
+
+    for(int i = 0; i < regions.size(); i++) {
+        auto region = std::next(regions.begin(), i);
+        barriers[i].sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+        barriers[i].srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barriers[i].dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+        barriers[i].offset = region->offset;
+        barriers[i].buffer = *region->buffer;
+        barriers[i].size = region->size();
+    }
+
+    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, 0, 0,
+                         VK_NULL_HANDLE, COUNT(barriers), barriers.data(), 0, VK_NULL_HANDLE);
+}
+
+void Barrier::transferWriteToWrite(VkCommandBuffer commandBuffer,
+                                   std::initializer_list<BufferRegion> regions) {
+    std::vector<VkBufferMemoryBarrier> barriers(regions.size());
+
+    for(int i = 0; i < regions.size(); i++) {
+        auto region = std::next(regions.begin(), i);
+        barriers[i].sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+        barriers[i].srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barriers[i].dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barriers[i].offset = region->offset;
+        barriers[i].buffer = *region->buffer;
+        barriers[i].size = region->size();
+    }
+
+    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0,
+                         VK_NULL_HANDLE, COUNT(barriers), barriers.data(), 0, VK_NULL_HANDLE);
+}
+
+void Barrier::transferReadToWrite(VkCommandBuffer commandBuffer,
+                                  std::initializer_list<BufferRegion> regions) {
+    std::vector<VkBufferMemoryBarrier> barriers(regions.size());
+
+    for(int i = 0; i < regions.size(); i++) {
+        auto region = std::next(regions.begin(), i);
+        barriers[i].sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+        barriers[i].srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+        barriers[i].dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barriers[i].offset = region->offset;
+        barriers[i].buffer = *region->buffer;
+        barriers[i].size = region->size();
+    }
+
+    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0,
+                         VK_NULL_HANDLE, COUNT(barriers), barriers.data(), 0, VK_NULL_HANDLE);
+}
+
+void Barrier::fragmentReadToComputeWrite(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions) {
+    std::vector<VkBufferMemoryBarrier> barriers(regions.size());
+
+    for(int i = 0; i < regions.size(); i++) {
+        auto region = std::next(regions.begin(), i);
+        barriers[i].sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+        barriers[i].srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+        barriers[i].dstAccessMask = VK_ACCESS_SHADER_WRITE_BIT;
+        barriers[i].offset = region->offset;
+        barriers[i].buffer = *region->buffer;
+        barriers[i].size = region->size();
+    }
+
+    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, 0, 0,
+                         VK_NULL_HANDLE, COUNT(barriers), barriers.data(), 0, VK_NULL_HANDLE);
+}
+
+void Barrier::computeWriteToFragmentRead(VkCommandBuffer commandBuffer, std::initializer_list<BufferRegion> regions) {
+    std::vector<VkBufferMemoryBarrier> barriers(regions.size());
+
+    for(int i = 0; i < regions.size(); i++) {
+        auto region = std::next(regions.begin(), i);
+        barriers[i].sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+        barriers[i].srcAccessMask = VK_ACCESS_SHADER_WRITE_BIT;
+        barriers[i].dstAccessMask = VK_ACCESS_SHADER_WRITE_BIT;
+        barriers[i].offset = region->offset;
+        barriers[i].buffer = *region->buffer;
+        barriers[i].size = region->size();
+    }
+
+    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0, 0,
+                         VK_NULL_HANDLE, COUNT(barriers), barriers.data(), 0, VK_NULL_HANDLE);
+}
