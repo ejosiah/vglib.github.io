c4e47470aacf77fcd36abefbc5fe92e2b175febd
Josiah Ebhomenye <joebhomenye@gmail.com>
2024-08-14 19:46:19 +0100

    add clip space graphics pipeline builder

--- VulkanBase/include/Prototypes.hpp
+++ VulkanBase/include/Prototypes.hpp
@@ -12,12 +12,18 @@ public:
     : _device(&device)
     , _swapchain(&_swapchain)
     , _renderPass(renderPass)
-    , _pipelineBuilder(device.graphicsPipelineBuilder()) {
+    , _pipelineBuilder(device.graphicsPipelineBuilder())
+    , _screenQuadPipelineBuilder(device.graphicsPipelineBuilder()) {
         initPipelineBuilderPrototype();
+        initScreenQuadPipelineBuilderPrototype();
     }
 
     GraphicsPipelineBuilder cloneGraphicsPipeline() const {
         return _pipelineBuilder.clone();
+    }
+
+    GraphicsPipelineBuilder cloneScreenSpaceGraphicsPipeline() const {
+        return _screenQuadPipelineBuilder.clone();
     }
 
 protected:
@@ -61,9 +67,48 @@ protected:
                 .name("gp_prototype");
     }
 
+    void initScreenQuadPipelineBuilderPrototype() {
+        _screenQuadPipelineBuilder
+            .allowDerivatives()
+            .vertexInputState()
+                .addVertexBindingDescriptions(ClipSpace::bindingDescription())
+                .addVertexAttributeDescriptions(ClipSpace::attributeDescriptions())
+            .inputAssemblyState()
+                .triangleStrip()
+            .viewportState()
+                .viewport()
+                    .origin(0, 0)
+                    .dimension(_swapchain->extent)
+                    .minDepth(0)
+                    .maxDepth(1)
+                .scissor()
+                    .offset(0, 0)
+                    .extent(_swapchain->extent)
+                .add()
+                .rasterizationState()
+                    .cullNone()
+                    .frontFaceCounterClockwise()
+                    .polygonModeFill()
+                .multisampleState()
+                    .rasterizationSamples(_device->settings.msaaSamples)
+                .depthStencilState()
+                    .enableDepthWrite()
+                    .enableDepthTest()
+                    .compareOpLess()
+                    .minDepthBounds(0)
+                    .maxDepthBounds(1)
+                .colorBlendState()
+                    .attachment()
+                    .add()
+                .renderPass(_renderPass)
+                .subpass(0)
+                .name("clip_gp_prototype");
+    }
+
 private:
     VulkanDevice* _device;
     VulkanSwapChain* _swapchain;
     VkRenderPass _renderPass;
     GraphicsPipelineBuilder _pipelineBuilder;
+    GraphicsPipelineBuilder _screenQuadPipelineBuilder;
 };
