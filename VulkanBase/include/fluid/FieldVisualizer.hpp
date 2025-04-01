ffcc8d588efca7742c2d24ff8089ead639baa5e5
Josiah Ebhomenye <joebhomenye@gmail.com>
2025-04-01 16:18:50 +0100

    Field visualizer in progress

--- VulkanBase/include/fluid/FieldVisualizer.hpp
+++ VulkanBase/include/fluid/FieldVisualizer.hpp
@@ -1,8 +1,25 @@
 #pragma once
 
 #include "Field.hpp"
+#include "ComputePipelins.hpp"
 
-class FieldVisualizer {
+class FieldVisualizer : ComputePipelines {
 public:
-    FieldVisualizer()
+    FieldVisualizer() = default;
+
+    FieldVisualizer(VulkanDevice* device, glm::ivec2 gridSize);
+
+    void init();
+
+    void add(eular::VectorField* vectorField);
+
+    void update(VkCommandBuffer commandBuffer);
+
+    void streamLines(VkCommandBuffer commandBuffer);
+
+private:
+    eular::VectorField* _vectorField{};
+    glm::ivec2 _gridSize;
+    VulkanBuffer _streamLines;
+
 };
---
+++ VulkanBase/source/FieldVisualizer.cpp
@@ -0,0 +1,2 @@
+#include "FieldVisualizer.hpp"
+
