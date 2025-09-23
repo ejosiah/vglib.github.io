bec7de091e2be49de34dd071930242c27ceec8e3
Josiah Ebhomenye <joebhomenye@gmail.com>
2025-09-23 04:07:42 +0100

    added integer typedefs

--- VulkanBase/include/common.h
+++ VulkanBase/include/common.h
@@ -67,6 +67,8 @@ namespace fs = std::filesystem;
 
 using real = float;
 using uint = unsigned int;
+using int64 = uint64_t;
+using uint64 = uint64_t;
 using Flags = unsigned int;
 using byte_string = std::vector<char>;
 using ubyte_string = std::vector<unsigned char>;
