41a0257b7a43e869d46248e7c8f1ccb1bdc62a5f
Josiah Ebhomenye <joebhomenye@gmail.com>
2025-10-10 02:28:30 +0100

    added flat map

--- VulkanBase/include/common.h
+++ VulkanBase/include/common.h
@@ -275,3 +275,14 @@ auto map_range(R&& range, Func&& func) {
     auto view = std::forward<R>(range) | std::views::transform(std::forward<Func>(func));
     return std::vector(view.begin(), view.end());
 }
+
+template <typename Range, typename Func>
+auto flat_map_range(Range&& range, Func&& func) {
+    auto joined =
+            std::forward<Range>(range)
+            | std::views::transform(std::forward<Func>(func))
+            | std::views::join;
+
+    using value_t = std::ranges::range_value_t<decltype(joined)>;
+    return std::vector<value_t>(std::ranges::begin(joined), std::ranges::end(joined));
+}
