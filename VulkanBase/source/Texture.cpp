f4265ed0497852f2043fc0be50f6bedd6cc9287b
Josiah Ebhomenye <joebhomenye@gmail.com>
2026-06-03 01:47:16 +0100

    setup up barrier based on source image layout

--- VulkanBase/source/Texture.cpp
+++ VulkanBase/source/Texture.cpp
@@ -1614,6 +1614,16 @@ void textures::generateLOD(const VulkanDevice& device, VulkanImage& image, uint3
 void textures::generateLOD(VkCommandBuffer commandBuffer, VulkanImage &image, uint32_t width, uint32_t height, uint32_t levels, uint32_t layers) {
     auto w = static_cast<int32_t>(width);
     auto h = static_cast<int32_t>(height);
+    const auto layout = image.currentLayout;
+    const auto srcAccessMask = layout == VK_IMAGE_LAYOUT_UNDEFINED
+        ? VkAccessFlags{ VK_ACCESS_NONE }
+        : VkAccessFlags{ VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT };
+    const auto srcStageMask = layout == VK_IMAGE_LAYOUT_UNDEFINED
+        ? VkPipelineStageFlags{ VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT }
+        : VkPipelineStageFlags{ VK_PIPELINE_STAGE_ALL_COMMANDS_BIT };
+    const auto dstAccessMask = layout == VK_IMAGE_LAYOUT_GENERAL
+        ? VkAccessFlags{ VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT }
+        : VkAccessFlags{ VK_ACCESS_SHADER_READ_BIT };
     VkImageBlit blit{};
     blit.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
     blit.srcSubresource.baseArrayLayer = 0;
@@ -1635,22 +1645,24 @@ void textures::generateLOD(VkCommandBuffer commandBuffer, VulkanImage &image, ui
         blit.srcOffsets[1] = {w, h, 1};
         blit.dstOffsets[1] = {w/2, h/2, 1};
 
-        barrier.srcAccessMask = VK_ACCESS_HOST_WRITE_BIT;
+        barrier.srcAccessMask = srcAccessMask;
         barrier.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
-        barrier.oldLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+        barrier.oldLayout = layout;
         barrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
         barrier.subresourceRange.baseMipLevel = 0;
         barrier.subresourceRange.levelCount = 1;
         barrier.subresourceRange.baseArrayLayer = layer;
         barrier.subresourceRange.layerCount = 1;
 
-        vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_HOST_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, 0, 0, 0, 1, &barrier);
+        vkCmdPipelineBarrier(commandBuffer, srcStageMask, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, 0, 0, 0, 1, &barrier);
 
+        barrier.srcAccessMask = srcAccessMask;
         barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barrier.oldLayout = layout;
         barrier.subresourceRange.baseMipLevel = 1;
         barrier.subresourceRange.levelCount = levels - 1;
         barrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-        vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_HOST_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, 0, 0, 0, 1, &barrier);
+        vkCmdPipelineBarrier(commandBuffer, srcStageMask, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, 0, 0, 0, 1, &barrier);
 
 
         barrier.subresourceRange.levelCount = 1;
@@ -1684,14 +1696,14 @@ void textures::generateLOD(VkCommandBuffer commandBuffer, VulkanImage &image, ui
     }
 
     barrier.srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
-    barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+    barrier.dstAccessMask = dstAccessMask;
     barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
-    barrier.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+    barrier.newLayout = layout;
     barrier.subresourceRange.baseMipLevel = 0;
     barrier.subresourceRange.levelCount = levels;
     barrier.subresourceRange.baseArrayLayer = 0;
     barrier.subresourceRange.layerCount = layers;
-    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0, 0, 0, 0, 0, 1, &barrier);
+    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, 0, 0, 0, 0, 0, 1, &barrier);
 }
 
 void textures::createDistribution(const VulkanDevice& device, const VulkanDescriptorPool& descriptorPool, const Texture& source, Texture& destination) {
